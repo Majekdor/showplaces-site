321318c18eec8bcbdc410dc531cefc09b0d24bfa
kevinbarnes <ksbarnes39@gmail.com>
2022-10-25 22:52:48 -0400

    Work on shared locations page

--- pages/shared.js
+++ pages/shared.js
@@ -1,17 +1,6 @@
 import {Map, MapkitProvider, Marker} from "react-mapkit";
 import Layout from "../components/layout";
-import {useEffect, useState} from "react";
-import {useSearchParams} from "next/dist/client/components/hooks-client";
-import {useRouter} from "next/router";
 import React from "react";
-
-function MapMarker(latitude, longitude, color, title, glyphText) {
-    this.latitude = latitude;
-    this.longitude = longitude;
-    this.color = color;
-    this.title = title;
-    this.glyphText = glyphText;
-}
 
 function SharedLocation(name, label, streetAddress, city, state, postalCode, country, latitude, longitude) {
     this.name = name;
@@ -25,63 +14,11 @@ function SharedLocation(name, label, streetAddress, city, state, postalCode, cou
     this.longitude = longitude;
 }
 
-export default function Shared() {
-    let sharedLocations = [];
-
-    const { query } = useRouter();
-    let origin = query.origin;
-
-    if (origin === "site") {
-        sharedLocations.push(
-            new SharedLocation(
-                query.name,
-                query.label,
-                query.streetAddress,
-                query.city,
-                query.state,
-                query.postalCode,
-                query.country,
-                query.latitude,
-                query.longitude
-            )
-        );
-    } else if (origin === "app") {
-        let contentId = query.contentId;
-        let [ json, setJson ] = useState(null);
-        let key = process.env.BYTEBIN_AUTH_KEY;
-        console.log(key);
-        fetch("https://bytebin.ocelotwrangler.com/" + contentId, { headers: {
-            Authorization: {key}
-        }})
-            //.then(response => console.log("response: " + response === undefined ? "null": response))
-            .then(response => response.json())
-            .then(result => {
-                //console.log(result);
-                setJson(result);
-            })
-
-        if (json === null) {
-            return "Loading...";
-        }
-
-        json["locations"].forEach(sharedLocation => {
-            console.log(sharedLocation)
-            sharedLocations.push(
-                new SharedLocation(
-                    sharedLocation["name"],
-                    sharedLocation["label"],
-                    sharedLocation["address"]["street"],
-                    sharedLocation["address"]["city"],
-                    sharedLocation["address"]["state"],
-                    sharedLocation["address"]["postalCode"],
-                    sharedLocation["address"]["country"],
-                    sharedLocation["latitude"],
-                    sharedLocation["longitude"]
-                )
-            );
-        });
-    }
-
+function Shared({ json }) {
+    let sharedLocations = JSON.parse(json["sharedLocations"])
+    let noSharedLocations = sharedLocations.length === 0
+    let latitudeCenter = json["latitudeCenter"]
+    let longitudeCenter = json["longitudeCenter"]
     return (
         <Layout title="Shared Locations">
             <header className="page__header">
@@ -90,12 +27,28 @@ export default function Shared() {
                 <p>This link can be opened in the Showplaces app.</p>
             </header>
             <div className="page__content">
+                <h2>Locations</h2>
+                <hr/>
+                {noSharedLocations ? (
+                    <p>No locations found from url.</p>
+                ) : (
+                    sharedLocations.map(location => (
+                        <div className="address">
+                            <h4>{location.name} | {location.label}</h4>
+                            <p>{location.streetAddress}</p>
+                            <p>{location.city}, {location.state} {location.postalCode}</p>
+                            <p>{location.country}</p>
+                            <br/>
+                        </div>
+                    ))
+                )}
+                <br/><br/>
                 <div id="map">
-                    <MapkitProvider tokenOrCallback={'/api/jwt'}>
+                    <MapkitProvider tokenOrCallback='/api/jwt'>
                         <Map
                             region={{
-                                latitude: 37.616934,
-                                longitude: -122.383790,
+                                latitude: latitudeCenter,
+                                longitude: longitudeCenter,
                                 latitudeSpan: 0.167647972,
                                 longitudeSpan: 0.354985255,
                             }}
@@ -110,7 +63,76 @@ export default function Shared() {
                         </Map>
                     </MapkitProvider>
                 </div>
+                <br/>
             </div>
         </Layout>
     )
 }
+
+export async function getServerSideProps(context) {
+    let sharedLocations = []
+
+    let query = context.query
+    let origin = query.origin
+
+    let latitudeTotal = 0
+    let longitudeTotal = 0
+    let latitudeCenter = 0
+    let longitudeCenter = 0
+
+    if (origin === "site") {
+        latitudeCenter = Number(query.latitude)
+        longitudeCenter = Number(query.longitude)
+        sharedLocations.push(
+            new SharedLocation(
+                query.name,
+                query.label,
+                query.streetAddress,
+                query.city,
+                query.state,
+                query.postalCode,
+                query.country,
+                Number(query.latitude),
+                Number(query.longitude)
+            )
+        )
+    } else if (origin === "app") {
+        let contentId = query.contentId
+        const response = await fetch("https://bytebin.ocelotwrangler.com/" + contentId, { headers: {
+                Authorization: process.env.BYTEBIN_AUTH_KEY
+            }}
+        )
+        const json = await response.json()
+
+        json["locations"].forEach(sharedLocation => {
+            latitudeTotal += sharedLocation["latitude"]
+            longitudeTotal += sharedLocation["longitude"]
+            sharedLocations.push(
+                new SharedLocation(
+                    sharedLocation["name"],
+                    sharedLocation["label"],
+                    sharedLocation["address"]["street"],
+                    sharedLocation["address"]["city"],
+                    sharedLocation["address"]["state"],
+                    sharedLocation["address"]["postalCode"],
+                    sharedLocation["address"]["country"],
+                    sharedLocation["latitude"],
+                    sharedLocation["longitude"]
+                )
+            )
+        })
+
+        latitudeCenter = latitudeTotal / sharedLocations.length
+        longitudeCenter = longitudeTotal / sharedLocations.length
+    }
+
+    let data = JSON.stringify(sharedLocations)
+    let json = {
+        "sharedLocations": data,
+        "latitudeCenter": latitudeCenter,
+        "longitudeCenter": longitudeCenter
+    }
+    return { props: { json }}
+}
+
+export default Shared
