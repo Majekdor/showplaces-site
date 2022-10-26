71a66280ad761c479c1b836e96cf33ba09deb7e5
Kevin Barnes <kevin@donniebarnes.com>
2022-10-26 10:17:32 -0400

    Add env vars to next config for aws amp

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
             //    destination: '/index.html'
             //}
         ]
+    },
+    env: {
+        MAPKIT_KEY: process.env.MAPKIT_KEY,
+        KID: process.env.KID,
+        ISS: process.env.ISS,
+        BYTEBIN_AUTH_KEY: process.env.BYTEBIN_AUTH_KEY
     }
 }
 
